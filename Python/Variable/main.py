e8929a9a45641e5c3cfc092398b3db46e3db93c5
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-08-20 23:08:49 -0400

    Create main.py

---
+++ Python/Variable/main.py
@@ -0,0 +1,10 @@
+# Variables
+name = "Kush"      # string
+age = 21           # integer
+height = 5.9       # float
+is_student = True  # boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
