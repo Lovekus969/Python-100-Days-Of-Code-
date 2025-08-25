2f0e95b05a9475d8dd754f32e132071c7aff516a
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-08-24 22:19:18 -0400

    Update variable.py

--- Python/Variable/variable.py
+++ Python/Variable/variable.py
@@ -1,3 +1,9 @@
 #we will see what variables we have
 first_name = "Kush" 
 print(first_name)
+
+print(" this is bhavneet here learning python")
+myname="bhavneet"
+email = "bhavneet11d@gmail.com"
+print(f"Hello {myname}")
+print(f" Your email Address is : {email}" )
