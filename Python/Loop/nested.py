911ed69b9e0681c2ea133c85f0a6184fc3f93e11
Bhav <Bhavneet11d@gmail.com>
2025-08-26 17:57:56 -0400

    Create nested.py

---
+++ Python/Loop/nested.py
@@ -0,0 +1,5 @@
+for outer_variable in range(1, 4):       # outer loop
+    print("Outer loop:", outer_variable)
+    
+    for inner_variable in range(1, 3):   # inner loop
+        print("   Inner loop:", inner_variable)
