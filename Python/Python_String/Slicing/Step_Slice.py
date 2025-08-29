1eea30a6d5af754c6dd099f0323e43e20025b538
Bhav <Bhavneet11d@gmail.com>
2025-08-29 10:07:18 -0400

    Create Step_Slice.py

---
+++ Python/Python_String/Slicing/Step_Slice.py
@@ -0,0 +1,5 @@
+s = "FAANG"
+print(s[::2])    # 'FAG'  → every 2nd character
+print(s[1::2])   # 'AN'   → every 2nd char starting at index 1
+print(s[::-1])   # 'GNAAF' → reversed string
+print(s[::-2])   # 'GA'   → reverse string, skipping every other char
