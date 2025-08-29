15d0fbe353932182ee6040f7890ad7580458e9bd
Bhav <Bhavneet11d@gmail.com>
2025-08-29 10:15:19 -0400

    Create work_with_spaces.py

---
+++ Python/Python_String/work_with_spaces.py
@@ -0,0 +1,4 @@
+text = "   hello world   "
+print(text.strip())     # "hello world"  (removes spaces from start & end)
+print(text.lstrip())    # "hello world   "
+print(text.rstrip())    # "   hello world"
