2a97b70dde6b06fbb59e46df4ebd3b8a1b0627b2
Bhav <Bhavneet11d@gmail.com>
2025-08-29 12:25:00 -0400

    Create format.py

---
+++ Python/Python_Methods/format.py
@@ -0,0 +1,8 @@
+name = "Kush"
+age = 25
+
+# Positional arguments
+print("My name is {} and I am {} years old".format(name, age))
+
+# Keyword arguments
+print("My name is {n} and I am {a} years old".format(n=name, a=age))
