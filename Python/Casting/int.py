2babac2610c3cbcdf136dfadd083bdcd25b71e16
Bhav <Bhavneet11d@gmail.com>
2025-08-29 09:52:47 -0400

    Create int.py

---
+++ Python/Casting/int.py
@@ -0,0 +1,6 @@
+x = int(1)       # 1
+y = int(2.9)     # 2  (decimal part removed)
+z = int("15")    # 15 (string converted to integer)
+
+# Invalid:
+# int("15.5")  ❌ Error (string must represent an integer only)
