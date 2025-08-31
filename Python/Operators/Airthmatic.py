a102cd3e42c50462ffd5ac242b5060a61777e1a5
Bhav <Bhavneet11d@gmail.com>
2025-08-31 14:37:27 -0400

    Create Airthmatic.py

---
+++ Python/Operators/Airthmatic.py
@@ -0,0 +1,10 @@
+x = 10
+y = 3
+
+print(x + y)   # 13 (Addition)
+print(x - y)   # 7 (Subtraction)
+print(x * y)   # 30 (Multiplication)
+print(x / y)   # 3.333... (Division)
+print(x % y)   # 1 (Modulus - remainder)
+print(x ** y)  # 1000 (Exponent - power)
+print(x // y)  # 3 (Floor division - no decimals)
