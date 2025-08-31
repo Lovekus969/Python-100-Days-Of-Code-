c9181805c5f2008eaa0e45fd4fa5fcf615a45eae
Bhav <Bhavneet11d@gmail.com>
2025-08-31 14:33:23 -0400

    Create operator.py

---
+++ Python/Data_Type/Boolean/operator.py
@@ -0,0 +1,14 @@
+print(True and True)    # True
+print(True and False)   # False
+
+
+
+#OR 
+
+print(False or True)    # True
+print(False or False)   # False
+
+#not
+print(not True)         # False
+print(not False)        # True
+
