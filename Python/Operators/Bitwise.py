ade5d2945679ec598f26631c0233d5a9f8517516
Bhav <Bhavneet11d@gmail.com>
2025-08-31 14:42:48 -0400

    Create Bitwise.py

---
+++ Python/Operators/Bitwise.py
@@ -0,0 +1,9 @@
+x = 6   # (110 in binary)
+y = 3   # (011 in binary)
+
+print(x & y)   # 2 (AND → 010)
+print(x | y)   # 7 (OR → 111)
+print(x ^ y)   # 5 (XOR → 101)
+print(~x)      # -7 (NOT → flips bits)
+print(x << 1)  # 12 (Left shift)
+print(x >> 1)  # 3 (Right shift)
