feea33180da484624112fdc90a9d21b8c352733c
Bhav <Bhavneet11d@gmail.com>
2025-08-31 14:28:09 -0400

    Create bool.py

---
+++ Python/Data_Type/Boolean/bool.py
@@ -0,0 +1,7 @@
+is_student = True
+has_job = False
+
+print(type(is_student))   # <class 'bool'>
+print(is_student and has_job)  # False
+print(is_student or has_job)   # True
+print(not is_student)          # False
